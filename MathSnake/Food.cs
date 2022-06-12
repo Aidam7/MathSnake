4effe09afea6c020e342c39419699279d60f5fc8
Adam Pečenka <55688345+Aidam7@users.noreply.github.com>
2022-06-12 11:43:59 +0200

    Food Implemented

---
+++ MathSnake/Food.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MathSnake
+{
+    public class Food
+    {
+        private bool _isFoodOnMap;
+
+        public bool IsFoodOnMap
+        {
+            get
+            {
+                return _isFoodOnMap;
+            }
+            set
+            {
+                _isFoodOnMap = value;
+            }
+        }
+        private Point _coordinates;
+        public Point Coordinates
+        {
+            get
+            {
+                return _coordinates;
+            }
+            set
+            {
+                _coordinates = value;
+            }
+        }
+
+        public Food(int X, int Y)
+        {
+            _coordinates.X = X;
+            _coordinates.Y = Y;
+        }
+    }
+}
